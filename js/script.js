6f84d8d4bdb68f4c6edf9f32cca280c86a01c684
post-coder <gabrielspanu.dev@gmail.com>
2024-02-07 12:55:33 +0100

    numeri casuali nella griglia

--- js/script.js
+++ js/script.js
@@ -11,12 +11,18 @@
 
 const gridElement = document.querySelector("#grid");
 
+
+const randomNumbersArray = getRandomNumbersArray();
+
+console.log(randomNumbersArray)
+
 // griglia 7x7
 for(let i = 0; i < 49; i++) {
 
     // creo un elemento, gli metto la classe "square" e lo aggiugno nella griglia
     const newElement = document.createElement("div");
     newElement.classList.add("square");
+    newElement.innerText = randomNumbersArray[i];
 
 
     // sto aggiungendo un event listener ad ogni quadrato che aggiungiamo
@@ -30,6 +36,8 @@ for(let i = 0; i < 49; i++) {
         // il this in questo contesto rappresenta il quadrato che è stato cliccato
         // di conseguenza possiamo manipolarlo a livello html come siamo abituati
         this.classList.toggle("active");
+
+        
     });
 
 
@@ -38,3 +46,39 @@ for(let i = 0; i < 49; i++) {
 }
 
 
+
+
+
+function getRandomNumbersArray() {
+
+    // ne diamo sempre 49
+
+    const numbersArray = [];
+
+    while (numbersArray.length < 49) {
+
+
+        // forza bruta: inserisci il numero solo se non è già presente
+        const newNumber = generateRandomNumber(49);
+
+        //controllo se il numero appena generato è già presente dentro il nostro array
+        if( ! numbersArray.includes(newNumber) ) {
+
+            numbersArray.push(newNumber);
+
+        }
+    }
+    // console.log(numbersArray)
+    return numbersArray;
+
+}
+
+
+
+// funzione che genera un numero random da 1 a 5
+function generateRandomNumber(maxNumber) {
+    const randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+
+    // restituisco il numero generato
+    return randomNumber;
+}
