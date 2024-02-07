bfff619d74609c2050383c436ea30957d142b9c1
post-coder <gabrielspanu.dev@gmail.com>
2024-02-07 12:03:39 +0100

    studio del this

--- js/script.js
+++ js/script.js
@@ -1,59 +1,19 @@
 
 
-// variabili globali
-// perchè sono state dichiarate fuori da delle parentesi graffe
-// di conseguenza sono accessibili ovunque
-const variabile = "testo";
+const button = document.querySelector("#button");
 
+button.addEventListener("click", function() {
+    // ci fa capire quale sia stato l'agente della sua chiamata, ovvero
+    // chi sia il "responsabile" della sua esecuzione
+    console.log(this);
+    // in questo caso infatti ci restituisce il pulsante che l'ha chiamato
+});
 
-if (1 < 2 ) {
-    console.log(variabile)
-
-    // variabili scope
-    // sono variabili che sono state inizializzate dentro delle graffe, 
-    // di conseguenza non sono leggibili e trovabili al di fuori
-    const variabile2 = "Testo 2";
-
-    console.log(variabile2);
-
-    if(true) {
-        console.log(variabile2)
-    }
-}
-
-// console.log(variabile2) //errore, la variabile non viene trovata
-
-
-
-for(let i = 0; i < 3; i++)  {
-    console.log(i);
-}
-
-// console.log(i) //errore, la variabile non viene trovata
-
-console.log("-------------------------")
-
-
-
-
-
-
-
-// ATTENZIONE
-// questa istruzione cancella tutto il contenuto della console, ci serve 
-// per avere più ordine, tranquilli, commentatelo per rivedere gli output precedenti
-console.clear();
-
-
-// come vediamo, l'utilizzo di una variabile "var" prima dell'inizializzazione non dà alcun errore
-console.log(parola);
-
-
-if(true) {
-    var parola = "testo";
-}
-
-// stessa cosa per l'utilizzo di una variabile che è stata creata in un altro scope
-console.log(parola)
-
+// this: chi è che ha chiamato questa azione che è stata appena eseguita?ù
+console.log(this);
+// this cambia valore di volta in volta in base al contesto
+// ovvero: in base a cosa l'abbia "chiamato"
+// in questo caso, il codice è stato eseguito come abbiamo chiesto alla finestra
+// del browser di ricaricare la pagina
+// di conseguenza il this = la finestra del browser
 
